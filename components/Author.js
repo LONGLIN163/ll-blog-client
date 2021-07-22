5f6e7a84b2f842d4d40f0a362d3afce64f8e96e5
developerlin123 <developerlin123@gmail.com>
2021-07-22 19:37:55 +0200

    change introduce

--- components/Author.js
+++ components/Author.js
@@ -9,10 +9,10 @@ const Author = () => {
                 <Avatar size={100} src="https://media-exp1.licdn.com/dms/image/C4D03AQEWfB9AxxGFGA/profile-displayphoto-shrink_400_400/0/1614599321512?e=1631145600&v=beta&t=EZ90iSmbU9sBmHrZjWZQQ6OP1TZ0cIeRrwuMq2XKEFw" />
             </div>
             <div className="author-introduction">
-            React, Hooks, Next.js, Redux, ES6, Angular1x, Node, Express, Koa, Egg.js, MongoDB, Mongoose, jQuery+Zepto+Underscore, Ajax, Bootstrap, Css, Less, Scss, Sass, Ejs, etc.
+                Me.See(WoCao Wuqing).then(Hello World!).then(WoW World).then(...)  
                 <Divider></Divider>
-                <Avatar size="28" icon={<GithubOutlined />} className="account"/>
-                <Avatar size="28" icon={<LinkedinOutlined />} className="account"/>
+                <Avatar size="28" icon={<GithubOutlined />} className="account" src="https://github.com/LONGLIN163"/>
+                <Avatar size="28" icon={<LinkedinOutlined />} className="account" src="https://www.linkedin.com/in/developerlin123/"/>
                 {/* <Avatar size="28" icon={<InstagramOutlined />} className="account"/> */}
             </div>
 
