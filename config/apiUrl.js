61fcd2b5b429a9532055948798e8be6e034b5cd2
linlong <developerlin123@gmail.com>
2023-02-10 15:47:37 +0100

    connect new server api

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,9 +1,12 @@
 
-/***prod api***/
+/***prod api heroku***/
 //let ipUrl="https://project-blog-service.herokuapp.com/blog/";
 
+/***prod api aws***/
+let ipUrl="http://blogservice-env.eba-iz3rabdz.us-east-1.elasticbeanstalk.com/blog/";
+
 /***dev api***/
-let ipUrl="http://localhost:8080/blog/";
+//let ipUrl="http://localhost:8080/blog/";
 
 let servicePath = {
     getArticleList:ipUrl + 'getArticleList' ,  //  apiurl-home page
@@ -12,5 +15,6 @@ let servicePath = {
     getListById:ipUrl + 'getListById/',  // apiurl-get article list by type id
 
 }
+
 export default servicePath;
 
