2bcced0451bcec32ab3c5c049a17e5edc8e85568
linlong <developerlin123@gmail.com>
2023-02-19 13:09:58 +0100

    commit image and ... on author page

--- components/Author.js
+++ components/Author.js
@@ -5,11 +5,11 @@ import {GithubOutlined,LinkedinOutlined} from "@ant-design/icons"
 const Author = () => {
     return ( 
         <div className="author-div comm-box">
-            <div>
+            {/* <div>
                 <Avatar size={100} src="https://media-exp1.licdn.com/dms/image/C4D03AQEWfB9AxxGFGA/profile-displayphoto-shrink_400_400/0/1614599321512?e=1665014400&v=beta&t=tcHfrIp_iUb3O0bX9EF39UQ-gWf6ErgD1hM50OaK3CA" />
-            </div>
+            </div> */}
             <div className="author-introduction">
-            I dreamed 127.0.0.1!!!!!!
+                learning, doing and testing......
             <Divider></Divider>
                 <a href="https://github.com/LONGLIN163">
                   <Avatar size="28" icon={<GithubOutlined />} className="account" />
